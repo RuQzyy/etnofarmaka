5c1ea74e79d35f6dd04c5cd718e14df745a2106e
Rahul <rahulboufakar@gmail.com>
2025-06-13 16:38:45 +0900

    kelupaan

--- database/migrations/2025_06_13_072847_create_item_pesanan.php
+++ database/migrations/2025_06_13_072847_create_item_pesanan.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('item_pesanan', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('id_pemesanan')->constrained('pemesanan')->onDelete('cascade');
+            $table->foreignId('id_obat')->constrained('obat')->onDelete('cascade');
+            $table->integer('jumlah')->default(1);
             $table->timestamps();
         });
     }
